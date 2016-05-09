3cea5001191d7f72924fd562f7afdebccb6da3ad
holyghost <thesedays@126.com>
2016-05-09 17:15:59 +0800

    add ZigZag Conversion

---
+++ leetcode/6.ZigZagConversion/Solution.java
@@ -0,0 +1,30 @@
+public class Solution {
+    public String convert(String s, int nRows) {
+        if (nRows == 1 || s.length() <= nRows) return s;
+
+        StringBuilder result = new StringBuilder("");
+
+        for (int i = 0; i < s.length(); i += nRows * 2 - 2) {
+            result.append(s.charAt(i));
+        }
+
+        for (int i = 1; i < nRows - 1; i++) {
+            int odd = i;
+            int even = nRows * 2 - 2 - i;
+            while (odd < s.length() || even < s.length()) {
+                result.append(s.charAt(odd));
+                if (even < s.length()) {
+                    result.append(s.charAt(even));
+                }
+                odd += nRows * 2 - 2;
+                even += nRows * 2 - 2;
+            }
+        }
+
+        for (int i = nRows - 1; i < s.length(); i += nRows * 2 - 2) {
+            result.append(s.charAt(i));
+        }
+
+        return result.toString();
+    }
+}

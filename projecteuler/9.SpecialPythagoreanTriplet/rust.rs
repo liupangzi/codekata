c9de336407272dc9f0077d5709bb50174522e808
liupangzi <thesedays@126.com>
2016-06-27 05:39:39 +0800

    add second solution

--- projecteuler/9.SpecialPythagoreanTriplet/rust.rs
+++ projecteuler/9.SpecialPythagoreanTriplet/rust.rs
@@ -1,8 +1,12 @@
 fn main() {
-    for a in 3..333 {
-        for b in a + 1..(1000 - 1 - a) / 2 {
-            let c = 1000 - a - b;
-            if c * c == a * a + b * b {
+    // a = (m^2 - n^2) ·d
+    // b = 2·m·n·d
+    // c = (m^2 + n^2) ·d
+    // m > n
+    for m in 2..Math.sqrt(500) {
+        for k in m + 1..500 / m {
+            // gcd(m, n) == 1 => gcd(m, m + n) == 1 => gcd(m, k) == 1
+            if (500 / m) % k == 0 && 1 == gcd(m, k) {
                 println!("a: {}, b: {}, c: {}", a, b ,c);
             }
         }

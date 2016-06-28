74ca3910a04f8177840f4447159424effde03ada
liupangzi <thesedays@126.com>
2016-06-29 00:26:43 +0800

    add 2nd solution of SummationOfPrimes

--- projecteuler/10.SummationOfPrimes/rust.rs
+++ projecteuler/10.SummationOfPrimes/rust.rs
@@ -1,84 +1,26 @@
 fn main() {
-    let s: i64 = 24;
-    let half_s: i64 = s / 2;
-    let m_limit: i64 = (s as f64).sqrt() as i64 + 1;
-    // let a = (m^2 - n^2) ·d
-    // let b = 2·m·n·d
-    // let c = (m^2 + n^2) ·d
-    // m > n && gcd(m, n) == 1
+    // sieve[i] == 1 stands for 2 * i + 1 is not prime
+    // sieve[i] == 0 stands for 2 * i + 1 is prime
+    let mut sieve = vec![0; 999999];
+    let i_limit: u32 = ((2000000 as f64).sqrt() as u32 - 1) / 2;
 
-    /* (1)
-     * 2 x d x m x (m + n) = s
-     *     => m x (m + n) = s / (2 x d)
-     *     => m^2 < s / (2 x d)
-     *     => m^2 < s / 2
-     *     => m <= round((s / 2)^0.5)
-     * (2)
-     * m > n > 0
-     *     => m >= 2
-     * (3)
-     * s / 2 = m x d x (m + n)
-     *     => (s / 2) % m == 0
-     */
-    for m in 2..m_limit {
-        if half_s % m == 0 {
-            /*
-             * (1)
-             * k = m + n
-             *     => k % 2 == 1
-             * (2)
-             * m < k = m + n < 2m
-             * (3)
-             * s / 2 = m x d x (m + n)
-             *     => k = (m + n) = (s / 2) / (m x d) < s / 2m
-             * 
-             */
-            let k_start = if m % 2 == 0 {
-                m + 1
-            } else {
-                m + 2
-            };
-
-            let mut half_s_div_m: i64 = s / (2 * m);
-            while half_s_div_m % 2 == 0 {
-                half_s_div_m = half_s_div_m / 2;
-            }
-
-            let k_end = if 2 * m  < half_s_div_m  {
-                2 * m
-            } else {
-                half_s_div_m + 1
-            };
-
-            for k in k_start..k_end {
-                // gcd(m, n) == 1 => gcd(m, m + n) == 1 => gcd(m, k) == 1
-                if (half_s/ m) % k == 0 && 1 == gcd(m, k) {
-                    let d: i64 = half_s / (m * k);
-                    let n: i64 = k - m;
-                    let a: i64 = (m * m - n * n) * d;
-                    let b: i64 = (2 * m * n) * d;
-                    let c: i64 = (m * m + n * n) * d;
-                    println!("a: {}, b: {}, c: {}", a, b ,c);
-                    break;
-                }
-                // k += 2;
+    for i in 1..i_limit {
+        let index = i as usize;
+        if sieve[index] == 0 {
+            let mut j: usize = 2 * index * (index + 1);
+            while j < 999999 {
+                sieve[j] = 1;
+                j = j + 2 * index + 1;
             }
         }
     }
-}
 
-fn gcd(x: i64, y: i64) -> i64 {
-    let (mut a, mut b) = if x > y {
-        (x, y)
-    } else {
-        (y, x)
-    };
-
-    while b != 0 {
-        let r = a % b;
-        a = b;
-        b = r;
+    let mut result: i64 = 2;
+    for k in 1..999999 {
+        if sieve[k] == 0 {
+            result = result + 2 * k as i64 + 1;
+        }
     }
 
-    a
+    println!("{}", result);
 }

f31383c54dacb530f72a4c38b6171859217b79c4
liupangzi <thesedays@126.com>
2016-06-30 23:48:52 +0800

    add rust solution for HighlyDivisibleTriangularNumber

--- projecteuler/12.HighlyDivisibleTriangularNumber/rust.rs
+++ projecteuler/12.HighlyDivisibleTriangularNumber/rust.rs
@@ -1,3 +1,23 @@
 fn main() {
-    println!("{}", result);
+    let mut i: i32 = 354;
+    loop {
+        let product: i32 = i * (i + 1) / 2;
+        let boundary = (product as f64).sqrt() as i32;
+        let mut count = 0;
+        let mut factor = 1;
+
+        while factor <= boundary {
+            if product % factor == 0 {
+                count = count + 1;
+            }
+            factor = factor + 1;
+        }
+
+        if count >= 250 {
+            println!("{}", i * (i + 1) / 2);
+            break;
+        }
+
+        i = i + 1;
+    }
 }

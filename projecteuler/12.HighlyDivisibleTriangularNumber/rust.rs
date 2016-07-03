f5041d88e1f5c9632b8ab02d9b6825eb81adb9b2
liupangzi <thesedays@126.com>
2016-07-03 12:21:26 +0800

    optimize rust solution for HighlyDivisibleTriangularNumber

--- projecteuler/12.HighlyDivisibleTriangularNumber/rust.rs
+++ projecteuler/12.HighlyDivisibleTriangularNumber/rust.rs
@@ -14,11 +14,24 @@ fn main() {
         }
     }
 
-    let mut i = 1;
+    let mut i = 6;
     let mut count = 0;
+    let mut keep_factor_count = 2;  // D(3) == 2;
     while count <= 500 {
-        count = 1;
-        let mut sum = i * (i + 1) / 2;
+        //println!("----------------------------------------");
+        //if i % 2 == 0 {
+        //    println!("D(({} * {}) / 2) = D({}) * D({})", i, i + 1, i / 2, i + 1);
+        //    println!("keep_factor_count aka D({}) is: {}", i / 2, keep_factor_count);
+        //} else {
+        //    println!("D(({} * {}) / 2) = D({}) * D({})", i, i + 1, (i + 1) / 2, i);
+        //    println!("keep_factor_count aka D({}) is: {}", i, keep_factor_count);
+        //}
+        let mut cal_factor_count = 1;
+        let mut sum = if i % 2 == 0 {
+            i + 1
+        } else {
+            (i + 1) / 2
+        };
 
         // sieve does not contain factor 2
         let mut carry_2 = 0;
@@ -26,28 +39,33 @@ fn main() {
             sum = sum / 2;
             carry_2 = carry_2 + 1;
         }
-        count = count * (carry_2 + 1);
+        cal_factor_count = cal_factor_count * (carry_2 + 1);
 
-        for j in 1..49999 {
-            if (2 * j + 1) * (2 * j + 1) > sum {
-                count = count * 2;
-                break;
-            }
-
-            let mut carry_exp = 0;
-            while sum % (2 * j + 1) == 0 {
-                sum = sum / (2 * j + 1);
-                carry_exp = carry_exp + 1;
-            }
-            count = count * (carry_exp + 1);
-
-            if sum == 1 {
-                break;
+        if sum > 1 {
+            for j in 1..49999 {
+                if (2 * j + 1) * (2 * j + 1) > sum {
+                    cal_factor_count = cal_factor_count * 2;
+                    break;
+                }
+            
+                let mut carry_exp = 0;
+                while sum % (2 * j + 1) == 0 {
+                    sum = sum / (2 * j + 1);
+                    carry_exp = carry_exp + 1;
+                }
+                cal_factor_count = cal_factor_count * (carry_exp + 1);
+            
+                if sum == 1 {
+                    break;
+                }
             }
         }
 
+        //println!("cal_factor_count: {}", cal_factor_count);
+        count = keep_factor_count * cal_factor_count;
+        keep_factor_count = cal_factor_count;
         i = i + 1;
     }
 
-    println!("{}", i * (i - 1) /2);
+    println!("{}", i * (i - 1) / 2);
 }

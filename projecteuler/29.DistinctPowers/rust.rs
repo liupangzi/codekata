37f5ffb9b78fb3d16c45823f4110790241603fca
刘超 <liuchao21@58ganji.com>
2016-08-18 17:41:21 +0800

    optimize algorithm

--- projecteuler/29.DistinctPowers/rust.rs
+++ projecteuler/29.DistinctPowers/rust.rs
@@ -23,13 +23,15 @@ fn main() {
     
     let max_base = (boundary as f64).sqrt() as i32;
     
+    // find max power(when base == 2) and max tmp_boundary(boundary's root)
     let mut max_power = 0;
     let mut tmp_boundary = boundary;
     while tmp_boundary >= 2 {
         tmp_boundary /= 2;
         max_power += 1;
     }
-    
+
+    // find the max power of a number that equals x ^ y
     let mut records: HashMap<i32, i32> = HashMap::new();
     for n in 2..max_power + 1 {
         for c in 2..max_base + 1 {
@@ -39,10 +41,22 @@ fn main() {
             }
         }
     }
-    
-    let mut result = (boundary - 1) * (boundary - 1);
+
+    // group by power, since get_duplicated() gets the same answer when tuple (boundary, power) is given
+    let mut counts: HashMap<i32, i32> = HashMap::new();
     for exp in records.values() {
-        result -= get_duplicated(boundary, *exp);
+        if counts.contains_key(exp) {
+            let tmp_count = counts[exp] + 1;
+            counts.insert(*exp, tmp_count);
+        } else {
+            counts.insert(*exp, 1);
+        }
+    }
+
+    // total
+    let mut result = (boundary - 1) * (boundary - 1);
+    for (power, power_count) in &counts {
+        result -= get_duplicated(boundary, *power) * power_count;
     }
 
     println!("{}", result);

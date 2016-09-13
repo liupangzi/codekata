42fcdf02b042fe1e090037e47ea363ea56b2b94a
刘超 <liuchao21@58ganji.com>
2016-09-13 14:36:18 +0800

    update #397

--- leetcode/Algorithms/397.IntegerReplacement/Solution.java
+++ leetcode/Algorithms/397.IntegerReplacement/Solution.java
@@ -2,6 +2,6 @@ public class Solution {
     public int integerReplacement(int n) {
         if (n == 1) return 0;
         if (n % 2 == 0) return integerReplacement(n / 2) + 1;
-        return Math.min(integerReplacement(n - 1) + 1, integerReplacement((n - 1) / 2 + 1) + 2);
+        return Math.min(integerReplacement((n - 1) / 2), integerReplacement((n - 1) / 2 + 1)) + 2;
     }
 }

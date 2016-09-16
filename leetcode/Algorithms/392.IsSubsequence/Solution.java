bda4b412f851c61ce442251cb72284f445556edf
liupangzi <thesedays@126.com>
2016-09-16 17:19:19 +0800

    refactor #392

--- leetcode/Algorithms/392.IsSubsequence/Solution.java
+++ leetcode/Algorithms/392.IsSubsequence/Solution.java
@@ -1,10 +1,9 @@
 public class Solution {
     public boolean isSubsequence(String s, String t) {
-        int i = 0;
-        int lastIndex = 0;
+        int i = 0, lastIndex = 0;
 
-        while (i < s.length() && t.indexOf(s.charAt(i), lastIndex) > -1) {
-            lastIndex = t.indexOf(s.charAt(i), lastIndex) + 1;
+        while (i < s.length() && (lastIndex = t.indexOf(s.charAt(i), lastIndex)) > -1) {
+            lastIndex++;
             i++;
         }
 

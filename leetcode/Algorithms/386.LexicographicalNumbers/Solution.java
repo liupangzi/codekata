0202d3d4fefa66fc3e0169bedc75f77df6b2ec60
liupangzi <thesedays@126.com>
2016-09-16 21:16:16 +0800

    refactor #386

--- leetcode/Algorithms/386.LexicographicalNumbers/Solution.java
+++ leetcode/Algorithms/386.LexicographicalNumbers/Solution.java
@@ -1,22 +1,18 @@
 public class Solution {
-    public List<Integer> lexicalOrder(int n) {
-        List<Integer> result = new ArrayList<>();
+    private List<Integer> result;
+    private int n;
 
-        int boundary = Math.min(n, 9);
-        for (int j = 1; j <= boundary; j++) {
-            result.add(j);
-            nextRound(result, j, n);
-        }
+    public List<Integer> lexicalOrder(int n) {
+        this.result = new ArrayList<>();
+        this.n = n;
+        nextRound(1);
 
         return result;
     }
-    
-    public static void nextRound(List<Integer> result, int current, int n) {
-        int count = 0;
-        while (count < 10 && (current * 10 + count) <= n) {
-            result.add(current * 10 + count);
-            nextRound(result, current * 10 + count, n);
-            count++;
-        }
+
+    private void nextRound(int current) {
+        result.add(current);
+        if (current * 10 <= n) nextRound(10 * current);
+        if (current < n && current % 10 < 9) nextRound(current + 1);
     }
 }
--- leetcode/Algorithms/386.LexicographicalNumbers/question.md
+++ leetcode/Algorithms/386.LexicographicalNumbers/question.md
@@ -1,4 +1,4 @@
-Given an integer n, return 1 - n in lexicographical order.
+Given an integer *n*, return 1 - *n* in lexicographical order.
 
 For example, given 13, return: [1,10,11,12,13,2,3,4,5,6,7,8,9].
 

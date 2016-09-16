db26630032ab7177687988d670ac258a189d7801
liupangzi <thesedays@126.com>
2016-09-16 13:10:40 +0800

    refactor TwoSum

--- leetcode/Algorithms/1.TwoSum/Solution.java
+++ leetcode/Algorithms/1.TwoSum/Solution.java
@@ -1,22 +1,14 @@
-import java.util.HashMap;
-
 public class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[] result = new int[2];
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
 
-        HashMap<Integer, Integer> data = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            int needle = target - nums[i];
-            if (data.containsKey(needle)) {
-                result[0] = data.get(needle);
-                result[1] = i;
-                break;
+            if (hashMap.get(target - nums[i]) != null) {
+                return new int[]{hashMap.get(target - nums[i]), i};
             }
-            else {
-                data.put(nums[i], i);
-            }
+            hashMap.put(nums[i], i);
         }
 
-        return result;
+        return new int[]{};
     }
 }

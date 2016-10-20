0d12189a353e679206c56a367fac8eaef104bede
刘超 <liuchao21@58ganji.com>
2016-10-20 11:30:23 +0800

    optimize #283

--- leetcode/Algorithms/283.MoveZeroes/Solution.java
+++ leetcode/Algorithms/283.MoveZeroes/Solution.java
@@ -1,15 +1,11 @@
 public class Solution {
     public void moveZeroes(int[] nums) {
-        int start = 0;
-        for (int n: nums) {
-            if (n != 0) {
-                nums[start] = n;
-                start++;
+        for (int start = 0, i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                int tmp = nums[start];
+                nums[start++] = nums[i];
+                nums[i] = tmp;
             }
-        }
-
-        for (; start < nums.length; start++) {
-            nums[start] = 0;
         }
     }
 }

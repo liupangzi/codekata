5438ce4a574e816c8e4ff6ac458dcc3cd2202b34
liupangzi <thesedays@126.com>
2016-10-23 22:24:24 +0800

    add #111

---
+++ leetcode/Algorithms/111.MinimumDepthOfBinaryTree/Solution.java
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode(int x) { val = x; }
+ * }
+ */
+
+public class Solution {
+    public int minDepth(TreeNode root) {
+        if (root == null) return 0;
+        if (root.left == null) return minDepth(root.right) + 1;
+        if (root.right == null) return minDepth(root.left) + 1;
+        return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+    }
+}

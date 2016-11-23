816663b30a0c1e82376055c9329ae942a7cf3f4c
刘超 <liuchao21@58ganji.com>
2016-11-23 18:09:43 +0800

    add #153

---
+++ leetcode/Algorithms/153.FindMinimumInRotatedSortedArray/Solution.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public int findMin(int[] nums) {
+        int low = 0, high = nums.length - 1;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] < nums[high]) {
+                high = mid;
+            } else if (nums[mid] > nums[high]) {
+                low = mid + 1;
+            }
+        }
+        return nums[low];
+    }
+}

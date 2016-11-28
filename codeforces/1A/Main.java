285fd084ef849f018b98678f3d31c67784bb3263
刘超 <liuchao21@58ganji.com>
2016-11-28 20:23:52 +0800

    optimized solution for #1A

--- codeforces/1A/Main.java
+++ codeforces/1A/Main.java
@@ -2,9 +2,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner cin = new Scanner(System.in);
-        long n = cin.nextLong(), m = cin.nextLong(), a = cin.nextLong();
-        long nBlocks = (n + a - 1) / a, mBlocks = (m + a - 1) / a;
-        System.out.println(nBlocks * mBlocks);
+        Scanner stdin = new Scanner(System.in);
+        long n = stdin.nextLong(), m = stdin.nextLong(), a = stdin.nextLong();
+        System.out.println(((n + a - 1) / a) * (m + a - 1) / a);
     }
 }

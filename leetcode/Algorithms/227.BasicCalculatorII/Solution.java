aebd4adbd8e62ef9a9a1ac1fa8f4f0132f28c0ca
刘超 <liuchao21@58ganji.com>
2016-12-16 01:10:11 +0800

    optimize using stack

--- leetcode/Algorithms/227.BasicCalculatorII/Solution.java
+++ leetcode/Algorithms/227.BasicCalculatorII/Solution.java
@@ -1,33 +1,25 @@
+import java.util.Stack;
+
 public class Solution {
     public int calculate(String s) {
-        StringBuilder stringBuilder = new StringBuilder();
-        int result = 0, start = 0, op = '+';
+        Stack<Integer> stack = new Stack<>();
+        int start = 0, op = '+', tmp = 0;
         while (start <= s.length()) {
-            if (start == s.length() || (s.charAt(start) == '+' || s.charAt(start) == '-')) {
-                if (op == '+') result += this.calc(stringBuilder);
-                if (op == '-') result -= this.calc(stringBuilder);
+            if (start == s.length() || s.charAt(start) == '+' || s.charAt(start) == '-' || s.charAt(start) == '*' || s.charAt(start) == '/') {
+                if (op == '+') stack.push(tmp);
+                if (op == '-') stack.push(-tmp);
+                if (op == '*') stack.push(stack.pop() * tmp);
+                if (op == '/') stack.push(stack.pop() / tmp);
+                tmp = 0;
                 if (start != s.length()) op = s.charAt(start);
-                stringBuilder = new StringBuilder();
             } else {
-                if (s.charAt(start) != ' ') stringBuilder.append(s.charAt(start));
+                if (s.charAt(start) != ' ') tmp = tmp * 10 + s.charAt(start) - '0';
             }
             start++;
         }
-        return result;
-    }
 
-    private int calc(StringBuilder sb) {
-        int result = 1, op = '*', tmp = 0;
-        for (int i = 0; i <= sb.length(); i++) {
-            if (i == sb.length() || sb.charAt(i) == '*' || sb.charAt(i) == '/') {
-                if (op == '*') result *= tmp;
-                if (op == '/') result /= tmp;
-                if (i != sb.length()) op = sb.charAt(i);
-                tmp = 0;
-            } else {
-                tmp = tmp * 10 + sb.charAt(i) - 48;
-            }
-        }
+        int result = 0;
+        for (int i : stack) result += i;
         return result;
     }
 }

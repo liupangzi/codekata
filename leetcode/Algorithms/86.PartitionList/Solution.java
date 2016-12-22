bc3607f4240070050ca5da77c674d1687e1a518d
liupangzi <thesedays@126.com>
2016-12-22 20:44:24 +0800

    fix code

--- leetcode/Algorithms/86.PartitionList/Solution.java
+++ leetcode/Algorithms/86.PartitionList/Solution.java
@@ -1,11 +1,11 @@
 /**
- *  * Definition for singly-linked list.
- *   * public class ListNode {
- *    *     int val;
- *     *     ListNode next;
- *      *     ListNode(int x) { val = x; }
- *       * }
- *        */
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
 
 public class Solution {
     public ListNode partition(ListNode head, int x) {

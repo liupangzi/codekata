3bcea39f5c920605465265f0dd562ebd4e8a21d8
liupangzi <thesedays@126.com>
2017-03-11 11:14:05 +0800

    add #450

---
+++ leetcode/Algorithms/450.DeleteNodeInABST/Solution.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if (root == null) return null;
+        if (root.val > key) {
+            root.left = deleteNode(root.left, key);
+        } else if (root.val < key) {
+            root.right = deleteNode(root.right, key);
+        } else {
+            if (root.left == null) return root.right;
+            if (root.right == null) return root.left;
+            TreeNode tmp = root.left;
+            root.left = null;
+            root = root.right;
+            TreeNode cursor = root;
+            while (cursor.left != null) cursor = cursor.left;
+            cursor.left = tmp;
+        }
+        return root;
+    }
+}

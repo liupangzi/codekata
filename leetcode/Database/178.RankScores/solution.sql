2633084189533b4eb35e2613b9f1c2ea86449a84
刘超 <liuchao21@58ganji.com>
2017-03-14 15:28:19 +0800

    better solution

--- leetcode/Database/178.RankScores/solution.sql
+++ leetcode/Database/178.RankScores/solution.sql
@@ -1,5 +1,3 @@
-SELECT s1.Score, s2.Rank FROM Scores s1,
-  (SELECT Score, @rownum := @rownum + 1 AS Rank
-    FROM (SELECT DISTINCT Score FROM Scores ORDER BY Score DESC) S, (SELECT @rownum := 0) r ORDER BY Score DESC
-  ) s2
-WHERE s1.Score = s2.Score ORDER BY Rank;
+SELECT Score, (SELECT count(*) FROM (SELECT DISTINCT Score s FROM Scores) tmp WHERE s >= Score) Rank
+  FROM Scores
+ORDER BY Score DESC

602e98191c0108a28c15e3dac36f1a30db643f90
liupangzi <thesedays@126.com>
2017-05-09 11:38:27 +0800

    add cycle detection for #287

---
+++ leetcode/Algorithms/287.FindTheDuplicateNumber/Solution.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def findDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        slow, fast = nums[0], nums[nums[0]]
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+
+        fast = 0
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[fast]
+
+        return slow

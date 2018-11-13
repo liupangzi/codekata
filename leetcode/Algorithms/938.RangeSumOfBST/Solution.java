1412fffa501cc8bf7e789b28ea1f31520988d2fc
liupangzi <thesedays@126.com>
2018-11-13 13:33:04 +0800

    add #938
    Change-Id: If6880c0a9965905db8c098fb4c39248ef6f8849f

---
+++ leetcode/Algorithms/938.RangeSumOfBST/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int rangeSumBST(TreeNode root, int L, int R) {
+        if (root == null) {
+            return 0;
+        } else if (root.val < L) {
+            return rangeSumBST(root.right, L, R);
+        } else if (root.val > R) {
+            return rangeSumBST(root.left, L, R);
+        } else {
+            return root.val + rangeSumBST(root.left, L, R) + rangeSumBST(root.right, L, R);
+        }
+    }
+}
